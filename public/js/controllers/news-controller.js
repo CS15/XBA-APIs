c95e82b0c9f06545f3998f814c3ffe5be0c55098
cs15 <twenty40@gmail.com>
2015-10-16 00:26:26 -0500

    Added Comments to news details.

--- public/js/controllers/news-controller.js
+++ public/js/controllers/news-controller.js
@@ -4,24 +4,11 @@
     angular.module('controllers').controller('NewsController', ['$scope', 'NewsServices',
         function($scope, NewsServices){
 
-            $scope.news = [
-                {
-                    title: "XBA Review: WRC 5",
-                    permalink: "news-22409-XBA-Review--WRC-5"
-                },
-                {
-                    title: "Call of Duty: Black Ops 3's Latest Campaign Vid Gets Tactical",
-                    permalink: "news-22408-Call-of-Duty--Black-Ops-3-s-Latest-Campaign-Vid-Gets-Tactical"
-                },
-                {
-                    title: "Halo 5: Guardians Now Available For Digital Pre-Order",
-                    permalink: "news-22407-Halo-5--Guardians-Now-Available-For-Digital-Pre-Order"
-                }
-            ];
-
-            $scope.selectedNews = $scope.news[0];
-
-            makeNetworkCall();
+            NewsServices.getLatestNews(1).then(function(response){
+                $scope.news = response.data;
+                $scope.selectedNews = $scope.news[0];
+                makeNetworkCall();
+            });
 
             $scope.update = function(){
                 makeNetworkCall();
