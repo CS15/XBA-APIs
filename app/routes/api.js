400bcc8f6363ae955dfaf28f6ae9fbaacdf5b5d1
cs15 <twenty40@gmail.com>
2015-10-16 00:16:35 -0500

    Added Comments to news details.

--- app/routes/api.js
+++ app/routes/api.js
@@ -55,9 +55,14 @@ module.exports = function (app, express) {
 
     api.get('/news/:permalink', function (req, res) {
 
-        var url = baseUrl + '/news/' + req.params.permalink + '.html';
+        var permalink = req.params.permalink;
+        var url = baseUrl + '/news/' + permalink + '.html';
+
+        var nID = permalink.substr(permalink.indexOf('-') + 1);
+        nID = nID.substr(0, nID.indexOf('-'));
 
         request(url, function (error, response, html) {
+            var self = this;
 
             if (error) return res.status(404).send(error);
 
@@ -65,25 +70,54 @@ module.exports = function (app, express) {
 
             var article = $('div.articleText');
 
-            var data = {};
+            self.data = {};
 
-            data.authorAvatar = $(article).find('table td[width=65] img').attr('src');
-            data.authorName = $(article).find('table td div.newsNFO span[itemprop=name]').text();
-            data.datePublished = $(article).find('table td div.newsNFO span[itemprop=datePublished]').text();
-            data.title = $(article).find('table td h1.newsTitle').text();
-            data.content = $(article).find('[itemprop=articleBody] p').text().trim();
-            data.images = [];
-            data.videos = [];
+            self.data.nID = nID;
+            self.data.authorAvatar = baseUrl + $(article).find('table td[width=65] img').attr('src');
+            self.data.authorName = $(article).find('table td div.newsNFO span[itemprop=name]').text();
+            self.data.datePublished = $(article).find('table td div.newsNFO span[itemprop=datePublished]').text();
+            self.data.title = $(article).find('table td h1.newsTitle').text();
+            self.data.content = $(article).find('[itemprop=articleBody] p').text().trim();
+            self.data.images = [];
+            self.data.videos = [];
+            self.data.comments = [];
 
-            $(article).find('[itemprop=articleBody] img').each(function(i, value){
-                data.images.push(value.attribs.src);
+            $(article).find('[itemprop=articleBody] img').each(function (i, value) {
+                self.data.images.push(value.attribs.src);
             });
 
-            $(article).find('[itemprop=articleBody] iframe').each(function(i, value){
-                data.videos.push(value.attribs.src);
+            $(article).find('[itemprop=articleBody] iframe').each(function (i, value) {
+                self.data.videos.push(value.attribs.src);
             });
 
-            res.status(200).send(data);
+            request.post({
+                url: 'http://www.xboxachievements.com/news2-loadcomments.php',
+                form: {nID: nID}
+            }, function (error, response, html) {
+                var $ = cheerio.load(html);
+
+                var comments = $('table tr');
+
+                $(comments).each(function(i, value){
+
+                    var author = $(value).find('td[width=334] a').eq(1).text().trim();
+                    var createdAt = $(value).find('td[width=334] .newsNFO').text().trim();
+                    var content = $(value).next().find('td[colspan=3]').text().trim();
+
+                    if (author) {
+                        var comment = {
+                            author: author,
+                            createdDate: createdAt,
+                            content: content
+                        };
+
+                        self.data.comments.push(comment);
+                    }
+                });
+
+                res.status(200).send(self.data);
+            });
+
         });
     });
 };
