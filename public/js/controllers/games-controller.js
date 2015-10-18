3b98ff511c16bb801b53dcb8d5cbb00c8aabd12e
cs15 <twenty40@gmail.com>
2015-10-18 12:17:19 -0500

    Working on tabs for games.

--- public/js/controllers/games-controller.js
+++ public/js/controllers/games-controller.js
@@ -1,26 +1,28 @@
 (function () {
     'use strict';
 
-    angular.module('controllers').controller('GamesController', ['$scope', 'ApiServices',
-        function ($scope, ApiServices) {
-            $scope.tabTemplate = '/views/achievements-partial.html';
-
-            ApiServices.getLatestAchievements(1).then(function (response) {
-                $scope.achievements = response.data;
-                $scope.selectedGame = $scope.achievements[0];
-                makeNetworkCall();
-            });
-
-            $scope.update = function () {
-                makeNetworkCall();
-            };
-
-            function makeNetworkCall() {
-                ApiServices.getAchievements($scope.selectedGame.permalink).then(function (response) {
-                    $scope.details = response.data;
-                });
-            }
+    angular.module('controllers').controller('GamesController', ['$scope',
+        function ($scope) {
+            $scope.tabAchievementsTemplate = '/views/achievements-partial.html';
         }]);
+
+    angular.module('controllers').controller('AchievementsController', ['$scope', 'ApiServices', function ($scope, ApiServices) {
+
+        ApiServices.getLatestAchievements(1).then(function (response) {
+            $scope.achievements = response.data;
+            $scope.selectedGame = $scope.achievements[0];
+
+            console.log($scope.selectedGame);
+
+            $scope.getAchievements();
+        });
+
+        $scope.getAchievements = function () {
+            ApiServices.getAchievements($scope.selectedGame.permalink).then(function (response) {
+                $scope.details = response.data;
+            });
+        };
+    }]);
 
     angular.module('controllers').controller('LatestAchievementsController', ['$scope', 'ApiServices',
         function ($scope, ApiServices) {
