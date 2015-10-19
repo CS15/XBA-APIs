ac8aa2a8dc265b5960209ec32a5eb4cfd192f699
Christian Soler <iamrelos@gmail.com>
2015-10-19 22:21:29 +0000

    ...

--- public/js/controllers/games-controller.js
+++ public/js/controllers/games-controller.js
@@ -4,31 +4,45 @@
     angular.module('controllers').controller('GamesController', ['$scope', 'ApiServices',
         function ($scope, ApiServices) {
             $scope.tabs = [
-                { title:'Achievements', templateUrl:'/views/achievements-partial.html' },
-                { title:'Screenshots', templateUrl: '/views/screenshots-partial.html' }
+                { id: 1, title:'Achievements', templateUrl:'/views/achievements-partial.html' },
+                { id: 2, title:'Screenshots', templateUrl: '/views/screenshots-partial.html' }
             ];
+            
+            $scope.currentId = $scope.tabs[0].id;
 
             ApiServices.getLatestAchievements().then(function (response) {
                 $scope.games = response.data;
                 $scope.selectedGame = $scope.games[0];
+                $scope.update(1);
             });
 
-            $scope.getAchievements = function () {
+            function getAchievements() {
+                $scope.achievements = undefined;
+                
                 ApiServices.getAchievements($scope.selectedGame.permalink).then(function (response) {
                     $scope.achievements = response.data;
                 });
             };
 
-            $scope.getScreenshots = function () {
+            function getScreenshots() {
+                $scope.screenshots = undefined;
+                
                 ApiServices.getScreenshots($scope.selectedGame.permalink).then(function (response) {
                     $scope.screenshots = response.data;
                 });
             };
 
-            $scope.update = function(index){
-                $scope.getAchievements();
-                $scope.getScreenshots();
-                console.log(index);
+            $scope.update = function(id){
+                $scope.currentId = id;
+                
+                switch (id) {
+                    case 1:
+                        getAchievements();
+                        break;
+                    case 2:
+                        getScreenshots();
+                        break;
+                }
             };
 
         }]);
