b2c1f7cd957d56aa0af452dd486bacf769e84b94
Christian Soler <iamrelos@gmail.com>
2015-11-17 22:51:04 +0000

    ...

--- public/js/controllers/game-info-controller.js
+++ public/js/controllers/game-info-controller.js
@@ -1,8 +1,8 @@
 (function () {
     'use strict';
 
-    angular.module('controllers').controller('GameInfoController', ['$route', '$location', '$routeParams', 'XaServices', 'ParseServices',
-        function ($route, $location, $routeParams, XaServices, ParseServices) {
+    angular.module('controllers').controller('GameInfoController', ['$location', '$routeParams', 'XaServices', 'ParseServices',
+        function ($location, $routeParams, XaServices, ParseServices) {
 
             if (!Parse.User.current()) $location.path('/');
 
@@ -10,47 +10,46 @@
 
             var vm = this;
 
-            XaServices.getGameInfo(gamePermalink).then(function(response){
-                vm.gameInfo = response;
-                
-                console.log(vm.gameInfo);
-
-                ParseServices.getGameInfo(vm.gameInfo.gameId).then(function(response){
-                    if (response.length > 0) {
-                        vm.inParse = true;
-                        vm.parseObjectId = response[0].objectId;
-                    }
-
-                    vm.gameInfoChecked = true;
-                });
-
-                XaServices.getGameAchievements(gamePermalink).then(function(response){
-                    vm.achievements = response;
-
-                    ParseServices.getGameAchievements(vm.gameInfo.gameId).then(function(response){
-                        vm.parseAchievements = response;
-
+            
+            vm.loadData = function(){
+                XaServices.getGameInfo(gamePermalink).then(function(response){
+                    vm.gameInfo = response;
+                    
+                    ParseServices.getGameInfo(vm.gameInfo.gameId).then(function(response){
                         if (response.length > 0) {
-                            angular.forEach(vm.achievements, function(value, key){
-
-                               for(var i in vm.parseAchievements) {
-                                   if (value.achievementId === response[i].achievementId) {
-                                       value.inParse = true;
-                                   }
-                               }
-                            });
+                            vm.inParse = true;
+                            vm.parseObjectId = response[0].objectId;
                         }
-
-                        vm.gameAchievementsChecked = true;
+    
+                        vm.gameInfoChecked = true;
+                    });
+    
+                    XaServices.getGameAchievements(gamePermalink).then(function(response){
+                        vm.achievements = response;
+    
+                        ParseServices.getGameAchievements(vm.gameInfo.gameId).then(function(response){
+                            vm.parseAchievements = response;
+    
+                            if (response.length > 0) {
+                                angular.forEach(vm.achievements, function(value, key){
+    
+                                   for(var i in vm.parseAchievements) {
+                                       if (value.achievementId === response[i].achievementId) {
+                                           value.inParse = true;
+                                       }
+                                   }
+                                });
+                            }
+    
+                            vm.gameAchievementsChecked = true;
+                        });
                     });
                 });
-            });
-
-
-
+            }
+            
             vm.addGameInfoToParse = function(game) {
                 ParseServices.updateGameInfo(game).then(function (response) {
-                    $route.reload();
+                    vm.loadData();
                 });
             };
 
@@ -63,12 +62,13 @@
                     });
 
                     ParseServices.uploadAchievements(vm.achievements).then(function (response){
-                        alert('Achievements Uploaded');
-                        $route.reload();
+                        vm.loadData();
                     }, function(error){
                         alert('Error');
                     });
                 }
             };
+            
+            vm.loadData();
     }]);
 })();
