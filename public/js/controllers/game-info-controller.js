d2dbe7bf72d6b38d69aaefff01c496c90873ee0f
Christian Soler <iamrelos@gmail.com>
2015-11-18 22:38:19 +0000

    ...

--- public/js/controllers/game-info-controller.js
+++ public/js/controllers/game-info-controller.js
@@ -1,73 +1,107 @@
-(function () {
+(function() {
     'use strict';
 
     angular.module('controllers').controller('GameInfoController', ['$location', '$routeParams', 'XaServices', 'ParseServices',
-        function ($location, $routeParams, XaServices, ParseServices) {
+        function($location, $routeParams, XaServices, ParseServices) {
 
             if (!Parse.User.current()) $location.path('/');
 
             var gamePermalink = $routeParams.permalink;
 
             var vm = this;
-            
-            vm.loadData = function(){
-                XaServices.getGameInfo(gamePermalink).then(function(response){
-                    vm.gameInfo = response;
-                    
-                    ParseServices.getGameInformation(vm.gameInfo.gameId).then(function(response){
+
+            function getGameInfoFromXA(permalink, callback) {
+                XaServices.getGameInfo(permalink)
+                    .then(function(response) {
+                        vm.gameInfo = response;
+                        
+                        callback(vm.gameInfo.gameId);
+                    }, function(error) {
+                        console.log(error);
+                    });
+            };
+
+            function getGameInfoFromParse(parseGameId) {
+                ParseServices.getGameInformation(parseGameId)
+                    .then(function(response) {
                         if (response.length > 0) {
                             vm.inParse = true;
                             vm.parseObjectId = response[0].objectId;
                         }
-    
+
                         vm.gameInfoChecked = true;
+                    }, function(error) {
+                        console.log(error);
                     });
-    
-                    XaServices.getGameAchievements(gamePermalink).then(function(response){
+            };
+
+            function getGameAchievementsFromXA(permalink, callback) {
+                XaServices.getGameAchievements(gamePermalink)
+                    .then(function(response) {
                         vm.achievements = response;
-    
-                        ParseServices.getGameAchievements(vm.gameInfo.gameId).then(function(response){
-                            vm.parseAchievements = response;
-                            
-                            if (response.length > 0) {
-                                angular.forEach(vm.achievements, function(value, key){
-    
-                                   for(var i in vm.parseAchievements) {
-                                       if (value.achievementId === response[i].achievementId) {
-                                           value.inParse = true;
-                                       }
-                                   }
-                                });
-                            }
-    
-                            vm.gameAchievementsChecked = true;
-                        });
+                        
+                        callback();
+                    }, function(error) {
+                        console.log(error);
                     });
-                });
-            }
-            
-            vm.addGameInfoToParse = function(game) {
-                ParseServices.addGameInfoToParse(game).then(function (response) {
-                    vm.loadData();
+            };
+
+            function getGameAchievementsFromParse(parseGameId) {
+                ParseServices.getGameAchievements(parseGameId).then(function(response) {
+                    vm.parseAchievements = response;
+
+                    if (response.length > 0) {
+                        angular.forEach(vm.achievements, function(value, key) {
+
+                            for (var i in vm.parseAchievements) {
+                                if (value.achievementId === response[i].achievementId) {
+                                    value.inParse = true;
+                                }
+                            }
+                        });
+                    }
+
+                    vm.gameAchievementsChecked = true;
+                }, function(error) {
+                    console.log(error);
                 });
             };
 
-            vm.uploadAllAchievements = function (){
+            function init() {
+                
+                getGameInfoFromXA(gamePermalink, function(gameId){
+                    getGameInfoFromParse(gameId);
+                    
+                    getGameAchievementsFromXA(gamePermalink, function(){
+                        getGameAchievementsFromParse(gameId);
+                    });
+                });
+                
+            };
+
+            vm.addGameInfoToParse = function(game) {
+                ParseServices.addGameInfoToParse(game).then(function(response) {
+                    init();
+                });
+            };
+
+            vm.uploadAllAchievements = function() {
                 if (vm.achievements.length > 0) {
 
-                    angular.forEach(vm.achievements, function(value, key){
+                    angular.forEach(vm.achievements, function(value, key) {
                         value.game = vm.parseObjectId;
                         value.gameId = vm.gameInfo.gameId;
                     });
 
-                    ParseServices.addAchievementsToParse(vm.achievements).then(function (response){
-                        vm.loadData();
-                    }, function(error){
+                    ParseServices.addAchievementsToParse(vm.achievements).then(function(response) {
+                        init();
+                    }, function(error) {
                         alert('Error');
                     });
                 }
             };
-            
-            vm.loadData();
-    }]);
+
+            init();
+        }
+    ]);
 })();
